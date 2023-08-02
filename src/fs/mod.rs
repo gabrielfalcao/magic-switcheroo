96744be99e845c8a06b4d2e547fe337c4612a4aa
Gabriel Falcão <gabriel@nacaolivre.org>
2023-08-02 00:47:42 +0000

    saving state

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -33,76 +33,76 @@ pub fn restore_file(filename: String, magic: String) -> Result<(), MSError> {
     write_file(filename, restored)?;
     Ok(())
 }
-// #[cfg(test)]
-// mod tests {
-//     use crate::errors::MSError;
-//     use super::enchant_file;
-//     use super::restore_file;
-//     use super::{read_file, write_file};
-//     use hex;
-//     use k9::{assert_equal};
+#[cfg(test)]
+mod tests {
+    use crate::errors::MSError;
+    use super::enchant_file;
+    use super::restore_file;
+    use super::{read_file, write_file};
+    use hex;
+    use k9::{assert_equal};
 
-//     fn test_image_data() -> Vec<u8> {
-//         Vec::from([
-//             // <car>
-//             0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d, 0x49, 0x48,
-//             0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x03, 0x00, 0x00,
-//             0x00, 0x28, 0xcb, 0x34,
-//             // </car>
-//             // <cdr>
-//             0xbb, 0x00, 0x00, 0x00, 0x03, 0x50, 0x4c, 0x54, 0x45, 0xff, 0xff, 0xff, 0xa7, 0xc4,
-//             0x1b, 0xc8, 0x00, 0x00, 0x00, 0x0a, 0x49, 0x44, 0x41, 0x54, 0x08, 0x99, 0x63, 0x60,
-//             0x00, 0x00, 0x00, 0x02, 0x00, 0x01, 0xf4, 0x71, 0x64, 0xa6, 0x00, 0x00, 0x00, 0x00,
-//             0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82,
-//             // </cdr>
-//         ])
-//     }
-//     fn create_test_image_file(filename: String) -> Result<String, MSError> {
-//         write_file(filename.clone(), test_image_data())?;
-//         Ok(filename)
-//     }
-//     #[test]
-//     fn test_enchant_file() -> Result<(), MSError> {
-//         let name: String = "to-enchant.png".to_string();
-//         let magic: String = "THISISMAGICO".to_string();
+    fn test_image_data() -> Vec<u8> {
+        Vec::from([
+            // <car>
+            0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d, 0x49, 0x48,
+            0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x03, 0x00, 0x00,
+            0x00, 0x28, 0xcb, 0x34,
+            // </car>
+            // <cdr>
+            0xbb, 0x00, 0x00, 0x00, 0x03, 0x50, 0x4c, 0x54, 0x45, 0xff, 0xff, 0xff, 0xa7, 0xc4,
+            0x1b, 0xc8, 0x00, 0x00, 0x00, 0x0a, 0x49, 0x44, 0x41, 0x54, 0x08, 0x99, 0x63, 0x60,
+            0x00, 0x00, 0x00, 0x02, 0x00, 0x01, 0xf4, 0x71, 0x64, 0xa6, 0x00, 0x00, 0x00, 0x00,
+            0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82,
+            // </cdr>
+        ])
+    }
+    fn create_test_image_file(filename: String) -> Result<String, MSError> {
+        write_file(filename.clone(), test_image_data())?;
+        Ok(filename)
+    }
+    #[test]
+    fn test_enchant_file() -> Result<(), MSError> {
+        let name: String = "to-enchant.png".to_string();
+        let magic: String = "THISISMAGICO".to_string();
 
-//         // Given an image file exists
-//         let filename = create_test_image_file(name.clone())?;
-//         assert_equal!("to-enchant.png", &filename);
+        // Given an image file exists
+        let filename = create_test_image_file(name.clone())?;
+        assert_equal!("to-enchant.png", &filename);
 
-//         // When I enchant it
-//         enchant_file(filename, magic.clone())?;
+        // When I enchant it
+        enchant_file(filename, magic.clone())?;
 
-//         // Then it should exist
-//         let (enchanted_contents, enchanted_checksum) = read_file(&name);
-//         assert_equal!("4b414078", hex::encode(enchanted_checksum));
-//         assert_equal!(
-//             hex::encode(enchanted_contents), "0c3d32245448495349534d414749434fc3bec3bfe0ffc57747e708639c7e1a7f34cb2800000003080100000001000000524448490d0000000a1a0a0d474e5089c3bec3bf826042ae444e454900000000a66471f401000200000060639908544144490a000000c81bc4a7ffffff45544c5003000000bb"
-//         );
+        // Then it should exist
+        let (enchanted_contents, enchanted_checksum) = read_file(&name)?;
+        assert_equal!(hex::encode(enchanted_checksum), "1e1e86f5");
+        assert_equal!(
+            hex::encode(enchanted_contents), "0000000c3d00000032245448495349534d414749434fc3bec3bfe0ffc57747e708639c7e1a7f34cb2800000003080100000001000000524448490d0000000a1a0a0d474e5089c3bec3bf826042ae444e454900000000a66471f401000200000060639908544144490a000000c81bc4a7ffffff45544c5003000000bb"
+        );
 
-//         Ok(())
-//     }
-//     #[test]
-//     fn test_restore_file() -> Result<(), MSError> {
-//         let name: String = "to-restore.png".to_string();
-//         let magic: String = "THISISMAGICO".to_string();
+        Ok(())
+    }
+    #[test]
+    fn test_restore_file() -> Result<(), MSError> {
+        let name: String = "to-restore.png".to_string();
+        let magic: String = "THISISMAGICO".to_string();
 
-//         // Given an image file exists
-//         let filename = create_test_image_file(name.clone())?;
-//         assert_equal!("to-restore.png", &filename);
+        // Given an image file exists
+        let filename = create_test_image_file(name.clone())?;
+        assert_equal!("to-restore.png", &filename);
 
-//         // And that it has been enchanted
-//         enchant_file(filename.clone(), magic.clone())?;
+        // And that it has been enchanted
+        enchant_file(filename.clone(), magic.clone())?;
 
-//         // When I restore it
-//         restore_file(filename.clone(), magic.clone())?;
+        // When I restore it
+        restore_file(filename.clone(), magic.clone())?;
 
-//         // Then it should have the previous contents
-//         let (read, _) = read_file(&filename);
-//         assert_equal!(
-//             hex::encode(read), hex::encode(test_image_data())
-//         );
+        // Then it should have the previous contents
+        let (read, _) = read_file(&filename)?;
+        assert_equal!(
+            hex::encode(read), hex::encode(test_image_data())
+        );
 
-//         Ok(())
-//     }
-// }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -45,23 +45,14 @@ impl fmt::Display for DigestMismatch {
 #[derive(Subcommand)]
 enum Commands {
     #[command(arg_required_else_help(true))]
-    Decode { filename: String },
-    #[command(arg_required_else_help(true))]
-    Switch {
+    Enchant {
         #[arg(short, long)]
         magic: String,
 
         filename: String,
     },
     #[command(arg_required_else_help(true))]
-    Jsonify {
-        #[arg(short, long)]
-        magic: String,
-
-        filename: String,
-    },
-    #[command(arg_required_else_help(true))]
-    Brush {
+    Repel {
         #[arg(short, long)]
         magic: String,
 
@@ -78,61 +69,13 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     let _bom = getmark();
 
     match &args.command {
-        Commands::Decode { filename } => {
-            let (raw, _digest) = read_file(filename)?;
-
-            let meta = MetaMagic::new(raw, "FOOBARBAZ137")?;
-            println!("{}: {}", filename, hex::encode(&meta.magic()));
-        }
-        Commands::Jsonify { magic, filename } => {
-            let (read, _) = read_file(filename)?;
-
-            let meta = MetaMagic::new(read, magic)?;
-            let json = serde_json::to_string_pretty(&meta)?;
-
-            write_file(filename.clone(), json.as_bytes().to_vec())?;
-        }
-        Commands::Switch { magic, filename } => {
+        Commands::Enchant { magic, filename } => {
             enchant_file(filename.to_string(), magic.to_string())?;
         }
-        Commands::Brush { filename, magic } => {
+        Commands::Repel { filename, magic } => {
             restore_file(filename.to_string(), magic.to_string())?;
         }
     }
 
     Ok(())
 }
-
-// #[cfg(test)]
-// mod e2e_tests {
-//     use k9::assert_equal;
-//     use magic_switcheroo::fs::enchant_file;
-//     use magic_switcheroo::fs::read_file;
-//     use magic_switcheroo::fs::restore_file;
-//     use std::error::Error;
-//     use std::path::Path;
-
-//     #[test]
-//     fn test_enchant_and_restore_file() -> Result<(), Box<dyn Error>> {
-//         let filename: String = "o-really.png".to_string();
-//         let magic: String = "THISISMAGICO".to_string();
-
-//         let (original_contents, original_checksum) = read_file(&filename);
-
-//         // Given an image file exists
-//         assert!(Path::new(&filename).exists());
-
-//         // When I enchant it
-//         enchant_file(filename.clone(), magic.clone())?;
-//         // And subsequently restore it
-//         restore_file(filename.clone(), magic.clone())?;
-
-//         // Then its checksum should match that of the original
-//         let (current_contents, current_checksum) = read_file(&filename);
-
-//         // Then its contents should also match that of the original
-//         assert_equal!(current_checksum, original_checksum);
-//         assert_equal!(current_contents, original_contents);
-//         Ok(())
-//     }
-// }
